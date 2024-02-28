3430ce3084179d7e92f8739d29a2627b682ac669
Yusheng Zhao <yushengzhao2020@outlook.com>
2024-02-28 19:37:42 +0800

    implement register with ADT

--- src/QIQC.jl
+++ src/QIQC.jl
@@ -1,10 +1,14 @@
 module QIQC
 
 using Yao
+using MLStyle
 
 export sdp_Choi_rep
 include("channel.jl")
 
 export variational_distance, sdp_measurement
 include("measurement.jl")
+
+
+include("registers.jl")
 end
